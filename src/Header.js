5bb23c8fd9c122f64bd2de834a4fee681ad7ca8b
roberto <robpalacios1@hotmail.com>
2020-07-30 20:51:02 -0500

    import and export Header

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Header() {
+    return <nav className="header">
+
+        { /* logo on the left */ }
+        <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" />
+        { /* search box */ }
+        { /* 3-links */}
+        { /* Shopping cart with number of items inside it*/}
+    </nav>
+}
+
+export default Header
+
