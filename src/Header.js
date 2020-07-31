7de0d6d4f12147063733e42d0279df03ebc871e7
roberto <robpalacios1@hotmail.com>
2020-07-30 21:20:39 -0500

    import Header.css

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,17 @@
 import React from 'react'
+import './Header.css';
 
 function Header() {
-    return <nav className="header">
-
+    return (
+        <nav className="header">
         { /* logo on the left */ }
-        <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" />
+        <img
+        className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="" />
         { /* search box */ }
         { /* 3-links */}
         { /* Shopping cart with number of items inside it*/}
     </nav>
+    );
 }
 
 export default Header
