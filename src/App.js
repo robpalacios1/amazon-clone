ea698aa1fe7f1c6d487f7bc0490a6d2fea283c27
roberto <robpalacios1@hotmail.com>
2020-07-30 20:14:04 -0500

    add the differents routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,45 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from  "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <h1>Lets go to build an Amazon Clone!!!!</h1>
-        <h2>Hello World</h2>
-
-        { /* We need a REACT-ROUTER */}
-
-
-        { /* localhost/ */ }
-        { /* localhost/checkout*/ }
-        { /* localhost/login*/ }
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/checkout">
+            <h1>Checkout</h1>
+          </Route>
+          <Route path="/login">
+            <h1>Login page</h1>
+          </Route>
+          {/* This is default route */}
+          <Route path="/">
+            <h1>Home Page!!!</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
+}
+
+{
+  /* We need a REACT-ROUTER */
+}
+
+
+{
+  /* localhost.com/ */
+}
+
+
+{
+  /* localhost/checkout*/
+}
+
+
+{
+  /* localhost/login*/
 }
 
 export default App;
