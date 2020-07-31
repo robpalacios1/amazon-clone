47f7195d36c57adcb69b01b294fc671fd69dd852
roberto <robpalacios1@hotmail.com>
2020-07-30 19:56:09 -0500

    install react router dom

--- src/App.js
+++ src/App.js
@@ -4,9 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>
-        Lets go to build an Amazon Clone!!!!
-      </h1>
+      <h1>Lets go to build an Amazon Clone!!!!</h1>
+        <h2>Hello World</h2>
+
+        { /* We need a REACT-ROUTER */}
+
+
+        { /* localhost/ */ }
+        { /* localhost/checkout*/ }
+        { /* localhost/login*/ }
     </div>
   );
 }
