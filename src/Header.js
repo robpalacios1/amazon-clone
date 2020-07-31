e094c427c62414a0816f6d811cea17f04e803e91
roberto <robpalacios1@hotmail.com>
2020-07-30 23:10:56 -0500

    add Links for each one

--- src/Header.js
+++ src/Header.js
@@ -6,23 +6,50 @@ import SearchIcon from '@material-ui/icons/Search';
 function Header() {
     return (
         <nav className="header">
-        { /* logo on the left */ }
-        <Link to="/">
-            <img className="header__logo"
-                src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-                alt=""
-            />
-        </Link>
+            { /* logo on the left */ }
+            <Link to="/">
+                <img className="header__logo"
+                    src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+                    alt=""
+                />
+            </Link>
 
-        { /* search box */ }
-        <div className="header__search">
-            <input type="text" className="header__searchInput" />
-            <SearchIcon className="header__searchIcon" />
-        </div>
+            { /* search box */ }
+            <div className="header__search">
+                <input type="text" className="header__searchInput" />
+                <SearchIcon className="header__searchIcon" />
+            </div>
 
-        { /* 3-links */}
-        { /* Shopping cart with number of items inside it*/}
-    </nav>
+            { /* 3-links */}
+            <div className="header__nav">
+            {/* 1st link */}
+            <Link to="/login" className="header__link">
+                <div className="header__option">
+                    <span className="header__optionLineOne">Hello Roberto</span>
+                    <span className="header__optionLineTwo">Sign In</span>
+                </div>
+            </Link>
+
+            {/* 2nd link */}
+            <Link to="/" className="header__link">
+                <div className="header__option">
+                    <span className="header__optionLineOne">Return</span>
+                    <span className="header__optionLineTwo">& Orders</span>
+                </div>
+            </Link>
+
+            {/* 3rd link */}
+            <Link to="/login" className="header__link">
+                <div className="header__option">
+                    <span className="header__optionLineOne">Your</span>
+                    <span className="header__optionLineTwo">Prime</span>
+                </div>
+            </Link>
+
+            {/* 4th link */}
+            </div>
+            { /* Shopping cart with number of items inside it*/}
+        </nav>
     );
 }
 
