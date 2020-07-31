64068f32a5d3e0d6271d635f8881a03bf1f983dd
roberto <robpalacios1@hotmail.com>
2020-07-30 22:04:08 -0500

    add input and componenent SearchIcon

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,25 @@
 import React from 'react'
 import './Header.css';
+import { Link } from 'react-router-dom';
+import SearchIcon from '@material-ui/icons/Search';
 
 function Header() {
     return (
         <nav className="header">
         { /* logo on the left */ }
-        <img
-        className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="" />
+        <Link to="/">
+            <img className="header__logo"
+                src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+                alt=""
+            />
+        </Link>
+
         { /* search box */ }
+        <div className="header__search">
+            <input type="text" className="header__searchInput" />
+            <SearchIcon className="header__searchIcon" />
+        </div>
+
         { /* 3-links */}
         { /* Shopping cart with number of items inside it*/}
     </nav>
