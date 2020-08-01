a27e4f89ee761d335cc7644d6000b056ad704726
roberto <robpalacios1@hotmail.com>
2020-08-01 15:13:21 -0500

    add Subtotal Component to right sideon screen

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,10 +1,33 @@
 import React from 'react'
 import './Subtotal.css'
+import CurrencyFormat from 'react-currency-format'
+import { useStateValue } from './StateProvider'
+import { getCartTotal } from './reducer';
 
 function Subtotal() {
+    const [{ cart }, dispatch] = useStateValue();
+
     return (
         <div className="subtotal">
             {/** Price */}
+
+            <CurrencyFormat
+                renderText={(value) => (
+                    <>
+                        <p>
+                            Subtotal ({cart.length} items): <strong>{` ${value} `}</strong>
+                        </p>
+                        <small className="subtotal__gift">
+                            <input type="checkbox" /> This order contains a gift
+                        </small>
+                    </>
+                )}
+                decimalScale={2}
+                value={getCartTotal(cart)}
+                displayType={"text"}
+                thousandSeparator={true}
+                prefix={"$"}
+            />
             <button>Proceed to Chechout</button>
         </div>
     )
