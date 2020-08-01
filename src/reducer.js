7eeb63e528d57509ff1029f04a823d590e848ee1
roberto <robpalacios1@hotmail.com>
2020-08-01 02:08:33 -0500

    add programming logic for items

--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,16 @@
 export const initialState = {
-    cart: [],
+    cart: [
+        {
+            id: 12321341,
+            title:
+                "The Lean Startup: How Constant Innovation",
+            price: 11.96,
+            rating: 5,
+            image:
+                "https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_B01,204,203,200_.jpg"
+
+        },
+    ],
     user: null
 };
 
@@ -14,7 +25,27 @@ const reducer = (state, action) => {
             };
         case 'REMOVE_FROM_CART':
             //Logic for removing items from cart
-            return { state }
+
+            //we cloned the basket
+            let newCart = [...state.cart];
+
+            const index = state.cart.findIndex(
+                (cartItem) => cartItem.id === action.id
+            );
+
+            if (index >= 0) {
+                // item exists in basket, remove it...
+                newCart.splice(index, 1);
+            } else {
+                console.warn(
+                    `Cant remove product (id: $(action.id)) as its not in cart`
+                );
+            }
+
+            return {
+                ...state,
+                cart: newCart,
+            };
         default:
             return state;
     }
