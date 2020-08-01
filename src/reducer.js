b67cd1e773995c1ee50be4f94544b9e9c6da1b44
roberto <robpalacios1@hotmail.com>
2020-08-01 15:14:37 -0500

    add const getCartTotal to do the amount

--- src/reducer.js
+++ src/reducer.js
@@ -14,6 +14,9 @@ export const initialState = {
     user: null
 };
 
+export const getCartTotal = (cart) =>
+    cart?.reduce((amount, item) => item.price + amount, 0);
+
 const reducer = (state, action) => {
     console.log(action)
     switch(action.type) {
