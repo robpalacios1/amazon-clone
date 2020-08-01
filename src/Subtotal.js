d47e220599919556db98ce6a9aebd0b16b4788be
roberto <robpalacios1@hotmail.com>
2020-08-01 12:14:25 -0500

    add Subtotal.js component

---
+++ src/Subtotal.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Subtotal.css'
+
+function Subtotal() {
+    return (
+        <div className="subtotal">
+            {/** Price */}
+            <button>Proceed to Chechout</button>
+        </div>
+    )
+}
+
+export default Subtotal
