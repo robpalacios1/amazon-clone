1994e0f953dbd011a8a9e8b3995f0f5d50d72044
roberto <robpalacios1@hotmail.com>
2020-08-01 20:32:35 -0500

    add authUser

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from  "react-router-dom"
 import Header from './Header';
 import Home from './Home'
 import Checkout from './Checkout'
+import Login from './Login'
+import { useStateValue } from './StateProvider'
+import { auth } from './firebase'
 
 function App() {
+  const [{ cart }, dispatch] = useStateValue();
+
+  // useEffect <<<<<<<< POWERFUL
+  // Piece of code which runs based on a given condition
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        // the user is logged in
+        dispatch({
+          type: "SET_USER",
+          user: authUser
+        })
+      } else {
+        // the user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null
+        })
+      }
+    })
+  }, []);
+
   return (
     <Router>
       <div className="App">
@@ -15,7 +41,7 @@ function App() {
             <Checkout />
           </Route>
           <Route path="/login">
-            <h1>Login page</h1>
+            <Login/>
           </Route>
           {/* This is default route */}
           <Route path="/">
