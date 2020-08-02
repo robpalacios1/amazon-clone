5d94e2eb9070eb51fcffe519b62c51bcada07e29
roberto <robpalacios1@hotmail.com>
2020-08-01 21:39:21 -0500

    import auth from firebase

--- src/App.js
+++ src/App.js
@@ -9,27 +9,34 @@ import { useStateValue } from './StateProvider'
 import { auth } from './firebase'
 
 function App() {
-  const [{ cart }, dispatch] = useStateValue();
+  const [{ user }, dispatch] = useStateValue();
 
   // useEffect <<<<<<<< POWERFUL
   // Piece of code which runs based on a given condition
 
   useEffect(() => {
-    auth.onAuthStateChanged((authUser) => {
+    const unsuscribe = auth.onAuthStateChanged((authUser) => {
       if (authUser) {
         // the user is logged in
+
         dispatch({
           type: "SET_USER",
           user: authUser
         })
       } else {
         // the user is logged out
+
         dispatch({
           type: "SET_USER",
           user: null
         })
       }
     })
+
+    return () => {
+      //Any cleanup operations go in here...
+      unsuscribe();
+    }
   }, []);
 
   return (
